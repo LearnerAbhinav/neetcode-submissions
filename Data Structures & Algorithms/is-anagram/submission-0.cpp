3a08a4142bc01f3dbe605691b08a406ac28b4f3d
LearnerAbhinav <abhinavsharma75990@gmail.com>
2026-09-24 23:55:10 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char, int> s1;
+        unordered_map<char, int> s2;
+
+        for(int i=0;i<s.size();i++){
+            s1[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            s2[t[i]]++;
+        }
+        
+        return s1 == s2;
+    }
+};
